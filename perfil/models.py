c400a1b623d40a18a360ba22744b994d1f383111
João Pedro Pessôa Primo <jppessoaprimo@gmail.com>
2025-09-24 17:53:29 -0300

    Aba de atividade

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from treinos.models import Treino
 
-# Create your models here.
+class Atividade(models.Model):
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    treino = models.ForeignKey(Treino, on_delete=models.CASCADE)
+    data_conclusao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.usuario.username} concluiu {self.treino.nome} em {self.data_conclusao.strftime('%d/%m/%Y')}"
+
+    class Meta:
+        ordering = ['-data_conclusao']
